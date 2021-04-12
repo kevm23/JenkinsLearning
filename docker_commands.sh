94035b8e7f15c0691670e3b5bc983badf6d95d1c
kevm23 <82215752+kevm23@users.noreply.github.com>
2021-04-12 16:32:22 +0100

    Update docker_commands.sh

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,9 +1,9 @@
 # prefix commands with sudo if user is not in docker group
 # build docker image
-docker build -t hands-on-pipeLearning/myapp .
+docker build -t pipelearning/myapp .
 
 # check docker image
-docker images | grep pipeLearning/myapp
+docker images | grep pipelearning/myapp
 
 # run docker container
-docker run -p 8888:5000 --name myapp pipeLearning/myapp
+docker run -p 8888:5000 --name myapp pipelearning/myapp
