89d4620e6d2a60be256360889eefdfafb0e70a99
kevm23 <82215752+kevm23@users.noreply.github.com>
2021-04-13 09:22:32 +0100

    Delete docker_commands.sh

--- docker_commands.sh
+++
@@ -1,9 +0,0 @@
-# prefix commands with sudo if user is not in docker group
-# build docker image
-docker build -t pipelearning/myapp .
-
-# check docker image
-docker images | grep pipelearning/myapp
-
-# run docker container
-docker run -p 8888:5000 --name myapp pipelearning/myapp
